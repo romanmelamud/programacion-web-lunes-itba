4ecc4d2ef64fa7ed41fecdc1536971ec3a95d395
romanmelamud <rmelamud@itba.edu.ar>
2024-05-02 09:43:03 -0300

    g

--- cronometro.js
+++ cronometro.js
@@ -26,6 +26,6 @@ window.onload = function () {
     iniciarCronometroEmpleado("empleado1", new Date("2005-11-29T00:00:00Z").getTime());
     iniciarCronometroEmpleado("empleado2", new Date("2015-01-29T00:00:00Z").getTime());
     iniciarCronometroEmpleado("empleado3", new Date("1999-08-29T00:00:00Z").getTime());
-    iniciarCronometroEmpleado("empleado4", new Date("2002-11-29T00:00:00Z").getTime());
+    iniciarCronometroEmpleado("empleado4", new Date("2002-01-05T14:14:00Z").getTime());
     // En el caso de empleado2, no se especifica fecha de inicio, por lo que no se mostrará el cronómetro
 };
